2795b41cc9e52b8637849a40e8460896831f0f98
s4ru <davidriveracordo@hotmail.com>
2023-08-22 20:23:08 -0500

    yesyesyes

--- My project/Assets/AuthHandler.cs
+++ My project/Assets/AuthHandler.cs
@@ -7,7 +7,7 @@ using UnityEngine.Networking;
 public class AuthHandler : MonoBehaviour
 {
 
-    public string ApirURL = "https://sid-restapi.onrender.com/";
+    public string ApiURL = "https://sid-restapi.onrender.com/";
 
     TMP_InputField UsernameInputField;
     TMP_InputField PasswordInputField;
@@ -20,18 +20,30 @@ public class AuthHandler : MonoBehaviour
 
     public void Registrar()
     {
-        AuthData auhtData = new AuthData();
-        auhtData username = UsernameInputField.text;
-        auhtData password = PasswordInputField.text;
+        AuthData authData = new AuthData();
+        authData.username = UsernameInputField.text;
+        authData.password = PasswordInputField.text;
 
-        string json = JsonUtility.ToJson(auhtData);
+        string json = JsonUtility.ToJson(authData);
 
         StartCoroutine(SendRegister(json));
     }
 
+    public void Login()
+    {
+        AuthData authData = new AuthData();
+        authData.username = UsernameInputField.text;
+        authData.password = PasswordInputField.text;
+
+        string json = JsonUtility.ToJson(authData);
+
+        StartCoroutine(SendLogin(json));
+    }
+
+
     IEnumerator SendRegister(string json)
     {
-        UnityWebRequest request = UnityWebRequest.Put(ApirURL + "/usuarios",json);
+        UnityWebRequest request = UnityWebRequest.Put(ApiURL + "/usuarios",json);
         request.SetRequestHeader("Content-Type", "application/json");
         request.method = "POST";
         yield return request.SendWebRequest();
@@ -56,6 +68,34 @@ public class AuthHandler : MonoBehaviour
         }
     }
 
+
+    IEnumerator SendLogin(string json)
+    {
+        UnityWebRequest request = UnityWebRequest.Put(ApiURL + "auth/login", json);
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.method = "POST";
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError)
+        {
+            Debug.Log("NETWORK ERROR :" + request.error);
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+            if (request.responseCode == 200)
+            {
+                AuthData data = JsonUtility.FromJson<AuthData>(request.downloadHandler.text);
+
+                Debug.Log("Inicio sesion el usuario" + data.usuario.username);
+                Debug.Log(data.token);
+            }
+            else
+            {
+                Debug.Log(request.error);
+            }
+        }
+    }
 }
 
 [System.Serializable]
@@ -64,6 +104,7 @@ public class AuthData
     public string username;
     public string password;
     public UserData usuario;
+    public string token;
 }
 
 [System.Serializable]
